fba446078ec30280c5b3732c067d2fb4e21e0f9b
Aaron Ware <aaron@linchpin.com>
2023-08-11 12:55:08 -0400

    fix(NO-JIRA): Manifest misconfiguration for release please
    release-as: 3.0.0

--- blocks/build/index.asset.php
+++ blocks/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '9e2e337736e83e695ae2');
+<?php return array('dependencies' => array('lodash', 'wp-components', 'wp-core-data', 'wp-data', 'wp-edit-post', 'wp-element', 'wp-i18n', 'wp-plugins', 'wp-primitives'), 'version' => '46e1be774b7bcd0bbbc7');
